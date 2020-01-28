852b408031a2081f7df595b90708923f944c6162
Alexandra Pestruyeva <apestruy@gmail.com>
2020-01-28 03:42:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ let drivers = ["Milo", "Otis", "Garfield"];
 
 function destructivelyAppendDriver(name) {
   drivers.push(name)
+  return drivers
 }
 
 function destructivelyPrependDriver(name) {
   drivers.unshift(name)
+  return drivers
 }
